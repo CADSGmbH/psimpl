7714dec1110a45ad9a5f18d3366f2bdf32a693db
Benedikt Waidhofer <benedikt.waidhofer@cads-additive.com>
2022-02-16 13:02:48 +0100

    replaced it with std::bind, lambda other possible option

--- lib/psimpl.h
+++ lib/psimpl.h
@@ -437,7 +437,7 @@ namespace psimpl
             stats.max = static_cast <double> (*std::max_element (first, last));
             stats.sum = static_cast <double> (std::accumulate (first, last, init));
             stats.mean = stats.sum / count;
-            std::transform (first, last, first, std::bind2nd (std::minus <value_type> (), stats.mean));
+            std::transform (first, last, first, std::bind (std::minus <value_type> (), std::placeholders::_1, stats.mean));
             stats.std = std::sqrt (static_cast <double> (std::inner_product (first, last, first, init)) / count);
             return stats;
         }
